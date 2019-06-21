161d1a973dc94658fac66d0bcf6933d0c00c9607
bdhwan <founder@altamirasoft.com>
2019-06-21 16:21:26 +0900

    fix

--- docker.sh
+++ docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --no-cache --tag bdhwan/ionic-maria-pm2:1.0 .
-docker push bdhwan/ionic-maria-pm2:1.0
+docker build --no-cache --tag bdhwan/ionic-maria-pm2:1.3 .
+docker push bdhwan/ionic-maria-pm2:1.3
